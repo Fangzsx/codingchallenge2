a74b2dee88e6ba05bcb48522fe896269058bc583
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-22 09:37:09 +0800

    finish

--- 01-codingchallenge2/script.js
+++ 01-codingchallenge2/script.js
@@ -1,11 +1,25 @@
 function calcTip(bill){
-    if(bill > 50 && bill < 300){
-        console.log('15 %');
+    if(bill >= 50 && bill <= 300){
+        return bill * 0.15;
     }else{
-        console.log('20 %');
+        return bill * 0.20;
     }
 }
 
 calcTip(10);
 
-const bills = [125, 555, 44]
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]),];
+
+console.log(tips);
+
+function getBillPlusTip(bill, tip){
+    return bill + tip;
+}
+
+const totals = [
+    getBillPlusTip(bills[0], tips[0]),
+    getBillPlusTip(bills[1], tips[1]),
+    getBillPlusTip(bills[2], tips[2])];
+
+console.log(totals);
